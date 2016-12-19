770105b1311852aee83df1c7586e60b19b55f69d
VitaliyGolub <xaker.dnepr@gmail.com>
2016-12-19 13:05:22 +0200

    fire

--- main.js
+++ main.js
@@ -8,19 +8,60 @@
 // }
 
 function create() {
-  var srok = document.getElementById('srok').value;
-  var client = document.getElementById('client').value;
-  var manager = document.getElementById('manager').value;
-  var material = document.getElementById('material').value;
-  var kolvo = document.getElementById('kolvo').value;
+    var srok = document.getElementById('srok').value;
+    var client = document.getElementById('client').value;
+    var manager = document.getElementById('manager').value;
+    var material = document.getElementById('material').value;
+    var kolvo = document.getElementById('kolvo').value;
 
+    // var res = EncodeStr("Привет мир!");
+    // alert(res);
+    // alert(srok);
+    // alert(client);
+    // alert(manager);
+    // alert(material);
+    // alert(kolvo);
 
+    // alert("Заявка отправлена.");
 
-  // alert(srok);
-  // alert(client);
-  // alert(manager);
-  // alert(material);
-  // alert(kolvo);
+    // alert(Asc("П"));
 
-  alert("Заявка отправлена.");
+    var temp = "";
+
+    for (var i = 0; i < 256; i++) {
+      temp += i + " " + Chr(i) + "\r\n";
+    }
+    alert(temp);
+
+    // П = 207
+
+var charCode = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+
+}
+
+function EncodeStr(str) {
+    var result = "";
+    for (var i = 0; i < str.length; i++) {
+        result += d2h(str[i].charCodeAt(0));
+
+        alert(Asc("П"));
+
+    }
+    return result;
+}
+
+function h2d(h) {
+    return parseInt(h, 16);
+}
+
+function d2h(d) {
+    return d.toString(16);
+}
+
+function Asc(String) {
+    return String.charCodeAt(0);
+}
+
+function Chr(AsciiNum) {
+    return String.fromCharCode(AsciiNum);
 }
